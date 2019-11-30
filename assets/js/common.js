7beaacb7152e122dbd692597194b1064ede69a04
Kostya <pererva.kostya@outlook.com>
2019-11-30 13:48:32 +0200

    Сделал адаптив

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,24 +2,37 @@ function initCarousel() {
     var carouselPhoto = new Swiper('.style-icons-slider', {
         loop: true,
         speed: 800,
-        spaceBetween: 0,
-        slidesPerView: 'auto',
-        centeredSlides: true,
+        spaceBetween: 10,
+        slidesPerView: '1',
+        centeredSlides: false,
         navigation: {
             nextEl: '.style-icons-slider-navigation .button-navigation-next',
             prevEl: '.style-icons-slider-navigation .button-navigation-prev',
         },
+        breakpoints: {
+            768: {
+                spaceBetween: 0,
+                slidesPerView: 'auto',
+                centeredSlides: true,
+            }
+        }
     });
 
     var carouselMore = new Swiper('.slider-more-content', {
         loop: true,
         speed: 800,
-        spaceBetween: 15,
-        slidesPerView: '2',
+        spaceBetween: 10,
+        slidesPerView: '1',
         navigation: {
             nextEl: '.slider-more-content-navigation .button-navigation-next',
             prevEl: '.slider-more-content-navigation .button-navigation-prev',
         },
+        breakpoints: {
+            1152: {
+                slidesPerView: 2,
+                spaceBetween: 15
+            }
+        }
     });
 }
 
@@ -29,10 +42,18 @@ function changeImage() {
 
     var btnList = [].slice.call(document.querySelectorAll('.photo-years-slider-button__item'));
     var slideList = [].slice.call(document.querySelectorAll('.photo-years-slider__item'));
+    var tooltipList = [].slice.call(document.querySelectorAll('.tooltip'));
 
     var change = function () {
         btnList.forEach(function (btn, ind) {
             btn.slide = slideList[ind];
+
+            if (window.innerWidth < 768) {
+                appendTooltip(btn.slide, tooltipList[ind]);
+            } else {
+                appendTooltip(btn, tooltipList[ind]);
+            }
+
             btn.addEventListener('click', function () {
                 hideSlide();
                 btn.classList.add('active');
@@ -60,6 +81,10 @@ function changeImage() {
         });
     };
 
+    var appendTooltip = function (node, tooltip) {
+        node.appendChild(tooltip);
+    };
+
     var init = function () {
         change();
     };
@@ -70,3 +95,11 @@ function changeImage() {
 }
 
 changeImage().init();
+
+window.addEventListener('resize', function () {
+    if (window.innerWidth < 768) {
+        changeImage().init();
+    } else {
+        changeImage().init();
+    }
+});
